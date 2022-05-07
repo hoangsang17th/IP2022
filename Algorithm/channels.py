25825ef938c1ff084e4c52192069fa8360869373
Hoàng Sang <phsang49@gmail.com>
2022-05-07 14:40:52 +0700

    Hoàn thiện dự án IP với các tính năng cơ bản của xử lý ảnh

---
+++ Algorithm/channels.py
@@ -0,0 +1,44 @@
+import cv2
+import numpy as np
+
+
+class Channels:
+
+    def change_Color(self, img, red, green, blue, alpha):
+        img = cv2.convertScaleAbs(img, alpha=alpha*0.01)
+        b, g, r = cv2.split(img)
+
+        for r_value in r:
+            cv2.add(r_value, red, r_value)
+        for g_value in g:
+            cv2.add(g_value, green, g_value)
+        for b_value in b:
+            cv2.add(b_value, blue, b_value)
+
+        img = cv2.merge((b, g, r))
+        return img
+
+    def change_Gamma(self, img, value):
+        value = value*0.02
+        invGamma = 1.0 / value
+        table = np.array([((i / 255.0) ** invGamma) * 255
+                          for i in np.arange(0, 256)]).astype("uint8")
+
+        img = cv2.LUT(img, table)
+        return img
+
+    def change_Logarit(self, img, value):
+        # Là thao tác xử lý điểm ảnh trên ảnh có dạng s = c * log(1+ r)
+        # Với s là điểm ảnh đã xử lý, r >= 0 là điểm ảnh đầu vào, c hằng số
+        # Xem chi tiết tại IP3/Tr16
+        img = float(2) * cv2.log(1.0 + img)
+        return img
+
+    def change_Island(self, img, imgNotIsland, value):
+        # Đảo ảnh với ảnh có các giá trị mức xám trong vùng [0, L – 1]
+        # Là thao tác xử lý điểm ảnh trên ảnh có dạng s = (L – 1) – r
+        if(value == 1):
+            img = imgNotIsland
+        else:
+            img = 255 - img
+        return img
---
+++ README.md
@@ -0,0 +1,4 @@
+# Image-Processing
+
+pyuic5 -x UI/app.ui -o UI/app.py
+pyuic5 -x UI/app.ui -o UI/check.py
